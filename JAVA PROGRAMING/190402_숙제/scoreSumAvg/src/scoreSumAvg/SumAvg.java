c85742515c8208e08eba09a0c2a4663610125d9f
SMART-12 <zye5943@gmail.com>
2019-04-02 18:06:43 +0900

    fg

---
+++ JAVA PROGRAMING/190402_숙제/scoreSumAvg/src/scoreSumAvg/SumAvg.java
@@ -0,0 +1,31 @@
+package scoreSumAvg;
+
+import java.util.Scanner;
+
+public class SumAvg {
+	
+	int firstNumber;
+	int lastNumber;
+	int allSum =0;
+	double allAvg =0;
+	 
+	public void inputNumber() {
+	Scanner scan = Scanner(System.in);
+		
+	System.out.println("중간고사 성적을 입력하세요.");
+	this.firstNumber = scan.nextInt();
+	
+	System.out.println("기말고사 성적을 입력하세요.");
+	this.lastNumber = scan.nextInt();
+}
+	
+	public void SumAvg (allSum) {
+	this.allSum = this.allSum + allSum;
+	
+	
+	this.allAvg = this.allSum / 
+	
+}
+	
+}
+	
